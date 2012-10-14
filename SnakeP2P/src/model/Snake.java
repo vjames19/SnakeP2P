2b20179673ae8baa7b8f84184764afb64d403512
segonz <segonzalez@live.com>
2012-10-14 00:27:08 -0400

    Create Snake class

--- SnakeP2P/src/model/Snake.java
+++ SnakeP2P/src/model/Snake.java
@@ -1,16 +1,52 @@
 package model;
 
-public class Snake {
+import java.util.ArrayList;
+import java.util.List;
 
-	
-	private int x;
-	private int y;
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+public class Snake extends Cell {
+	public enum Orientation {
+		LEFT, RIGHT, DOWN, UP
 	}
 
+	private static final int INITIAL_SIZE = 4;
+	private List<Cell> body;
+	private Orientation direction;
+	
+	public Snake(int x, int y, int size, Orientation o) {
+		super(x, y, size);
+		this.body = new ArrayList<Cell>(4);
+		
+		if (o == Orientation.LEFT) {
+			for(int i = 0; i < INITIAL_SIZE; ++i) {
+				body.add( new Cell(x, y - i * size, size) );
+			}
+		}
+		else if (o == Orientation.RIGHT) {
+			for(int i = 0; i < INITIAL_SIZE; ++i) {
+				body.add( new Cell(x, y + i * size, size) );
+			}
+		}
+		else if (o == Orientation.DOWN) {
+			for(int i = 0; i < INITIAL_SIZE; ++i) {
+				body.add( new Cell(x - i * size, y, size) );
+			}
+		}
+		else if (o == Orientation.UP) {
+			for(int i = 0; i < INITIAL_SIZE; ++i) {
+				body.add( new Cell(x + i * size, y, size) );
+			}
+		}
+	}
+
+	public Orientation getDirection() {
+		return direction;
+	}
+
+	public void setDirection(Orientation direction) {
+		this.direction = direction;
+	}
+	
+	public int headPosition() {
+		return body.get(0);
+	}
 }
